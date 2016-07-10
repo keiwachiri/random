6de15244b2c575a981ff5fd187f552c151ba1d84
keiwachiri <keiwachiri@gmail.com>
2016-07-10 16:16:06 +0200

    Add valid_braces to work with parantheses, braces and brackets at the same time. Can to be refactored so it uses some kid of stack

--- valid_parentheses.py
+++ valid_parentheses.py
@@ -8,3 +8,27 @@ def valid_parentheses(string):
         if count < 0:
             return False
     return True if count == 0 else False
+
+
+def valid_braces(string):
+    # TODO - refactor to use dictionary and stack
+    count_parentheses = count_brackets = count_braces = 0
+    for char in string:
+        if char == '(':
+            count_parentheses += 1
+        elif char == ')':
+            count_parentheses -= 1
+        elif char == '[':
+            count_brackets += 1
+        elif char == ']':
+            count_brackets -= 1
+        elif char == '{':
+            count_braces += 1
+        elif char == '}':
+            count_braces -= 1
+        if count_brackets < 0 or count_parentheses < 0 or count_braces < 0:
+            return False
+    if (count_brackets, count_parentheses, count_braces) != (0, 0, 0):
+        return False
+    else:
+        return True
